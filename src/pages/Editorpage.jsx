3f9c8053d4bec15a4c6fce5b60c95c12ed9e4a53
vital01 <shobhitgpt01@gmail.com>
2023-01-20 02:19:43 +0530

    s1

---
+++ src/pages/Editorpage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Editorpage = () => {
+    return (
+        <div>Editorpage</div>
+    )
+}
+export default Editorpage;
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Signup = () => {
+    return (
+        <div><p>signup</p></div>
+    )
+}
+export default Signup;
