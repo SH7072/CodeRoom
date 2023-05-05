6dcfb93d3f0268c5b21d8cde40a28120d4b84d68
Shreyas Kasliwal <kasliwalshreyas07@gmail.com>
2023-05-05 16:33:22 +0530

    folder structure

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ import {
   Outlet,
   Routes,
 } from "react-router-dom";
-import Home from "./pages/Home"
-import Signup from "./pages/Signup"
-import Login from "./pages/Login"
-import Editorpage from './pages/Editorpage';
+import Home from "./components/Home"
+import Signup from "./components/Signup"
+import Login from "./components/Login"
+import Editorpage from './components/Editorpage';
 
 
 // const router = createBrowserRouter([
